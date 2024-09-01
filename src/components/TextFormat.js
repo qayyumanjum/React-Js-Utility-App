df9e3ad9a6b4f60e0a0e6fc5c4ede11dc0f669be
Muhammad Qayyum Anjum <130007451+qayyumanjum@users.noreply.github.com>
2024-09-01 17:59:51 +0500

    use speech text in format text area for formatting

--- src/components/TextFormat.js
+++ src/components/TextFormat.js
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function TextFormat(props) {
     const [text, setText] = useState('');
+
     const handleUpperCase = () => {
         let newText = text.toUpperCase();
         setText(newText);
         props.showAlert("Text is converted into Upper Case", "success");
+    }
 
+    useEffect(() => {
+        const storedText = localStorage.getItem('transcript');
+        if (storedText) {
+            setText(storedText);
+        }
+    }, []);
+
+    const handleLocalStorageChange = (event) => {
+        const updatedText = event.target.value;
+        setText(updatedText);
+        localStorage.setItem('transcript', updatedText);
     }
-    const OnChange = (event) => {
-        console.log('on Change Worked')
-        setText(event.target.value)
-    }
+
     const handleLowerCase = () => {
         let newText = text.toLowerCase();
         setText(newText);
         props.showAlert("Text is converted into Lower Case", "success");
-
     }
+
     const handleCopyCase = () => {
         navigator.clipboard.writeText(text);
         props.showAlert("Text is Copied to Clipboard", "success");
-
     }
+
     const capitalizeEveryWord = (str) => {
         let words = str.split(' ');
         for (let i = 0; i < words.length; i++) {
@@ -36,7 +46,6 @@ export default function TextFormat(props) {
         const capitalizedText = capitalizeEveryWord(text);
         setText(capitalizedText);
         props.showAlert("Text is converted into Capitalized Form", "success");
-
     };
 
     const handleClearCase = () => {
@@ -44,6 +53,7 @@ export default function TextFormat(props) {
         setText(newText);
         props.showAlert("Text is Cleared", "danger");
     }
+
     const wordCount = text.trim().split(/\s+/).filter(Boolean).length;
     const charCount = text.replace(/\s/g, "").length;
 
@@ -51,8 +61,16 @@ export default function TextFormat(props) {
         <>
             <div className="container">
                 <h1 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>{props.heading}</h1>
-                <div className="mb-2 ">
-                    <textarea className={`form-control no-resize  ${props.mode === 'dark' ? 'dark-placeholder' : 'light-placeholder'}`} id="textBox" rows="12" onChange={OnChange} value={text} placeholder='Enter Text Here...' style={{ backgroundColor: props.mode === 'dark' ? 'rgb(48 48 50)' : '#f9f9f9', color: props.mode === 'dark' ? '#f3f3f3' : '#45474B' }}></textarea>
+                <div className="mb-2">
+                    <textarea
+                        className={`form-control no-resize ${props.mode === 'dark' ? 'dark-placeholder' : 'light-placeholder'}`}
+                        id="textBox"
+                        rows="12"
+                        onChange={handleLocalStorageChange}
+                        value={text}
+                        placeholder='Enter Text Here...'
+                        style={{ backgroundColor: props.mode === 'dark' ? 'rgb(48 48 50)' : '#f9f9f9', color: props.mode === 'dark' ? '#f3f3f3' : '#45474B' }}
+                    ></textarea>
                 </div>
                 <button disabled={text.length === 0} className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'} mx-1 my-1`} onClick={handleCapitalize}>Capitalize Word</button>
                 <button disabled={text.length === 0} className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'} mx-1 my-1`} onClick={handleLowerCase}>Convert to Lower-Case</button>
@@ -60,14 +78,13 @@ export default function TextFormat(props) {
                 <button disabled={text.length === 0} className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'} mx-1 my-1`} onClick={handleCopyCase}>Copy Text</button>
                 <button disabled={text.length === 0} className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'} mx-1 my-1`} onClick={handleClearCase}>Clear Text</button>
                 <NavLink to="/Speech_Recognizer">
-                <button disabled={text.length === 0} className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'} mx-1 my-1`}>Speech to Text</button>
+                    <button className={`btn btn-${props.mode === 'light' ? 'dark' : 'light'} mx-1 my-1`}>Speech to Text</button>
                 </NavLink>
             </div>
             <div className="container mx-1 mb-6">
                 <h1 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>Your Text Summary</h1>
-                <p className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}  >{wordCount} {wordCount === 1 ? 'Word' : 'Words'} And {charCount} {charCount === 1 ? 'Character' : 'Characters'}</p>
+                <p className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}>{wordCount} {wordCount === 1 ? 'Word' : 'Words'} And {charCount} {charCount === 1 ? 'Character' : 'Characters'}</p>
             </div>
         </>
     )
 }
-
